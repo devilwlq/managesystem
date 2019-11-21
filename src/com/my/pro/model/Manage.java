741d553071218d869654325d8c0c019b163c5f4a
devilwlq <57120676+devilwlq@users.noreply.github.com>
2019-11-21 22:50:11 +0800

    pojo

--- src/com/my/pro/model/Manage.java
+++ src/com/my/pro/model/Manage.java
@@ -1,11 +1,8 @@
 package com.my.pro.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
--- src/com/my/pro/model/User.java
+++ src/com/my/pro/model/User.java
@@ -2,19 +2,15 @@ package com.my.pro.model;
 
 import java.io.Serializable;
 import java.util.Date;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
-import org.springframework.transaction.annotation.Transactional;
+
+
 
 @Entity
 @Table(name = "user")
